3213c109b14455acef172b1ceb5a3c040ed556c2
carlosvfb <cacavfb2107@gmail.com>
2025-01-19 21:43:18 -0300

    adicionando qrcode

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const QRCode = require('qrcode');
 
 const app = express();
 app.use(cors());
@@ -29,7 +30,8 @@ const musicas = [
     Quis saber pra onde vou
     Eis-me aqui
     Minha dor serenou.`,
-    "url": "https://www.cifraclub.com.br/tim-vanessa/aos-pes-do-monte/"
+    "url": "https://www.cifraclub.com.br/tim-vanessa/aos-pes-do-monte/",
+    "urlQrCode": "https://www.cifraclub.com.br/tim-vanessa/aos-pes-do-monte/"
   }
 ];
 
@@ -45,6 +47,23 @@ app.get('/musicas/:nome', (req, res) => {
 
   if (musica) {
     res.json(musica);
+  } else {
+    res.status(404).send('Música não encontrada');
+  }
+});
+
+app.get('/musicas/:nome/qrcode', async (req, res) => {
+  const nomeMusica = req.params.nome.toLowerCase();
+  const musica = musicas.find(m => m.nome.toLowerCase() === nomeMusica);
+
+  if (musica) {
+    try {
+      const qrCodeDataUrl = await QRCode.toDataURL(musica.urlQrCode); // Link da música no site
+      res.json({ nome: musica.nome, qrCode: qrCodeDataUrl });
+    } catch (error) {
+      console.error('Erro ao gerar QR Code:', error);
+      res.status(500).send('Erro ao gerar QR Code');
+    }
   } else {
     res.status(404).send('Música não encontrada');
   }
